6705a3af198b3b8c02f7d75b31f5a659cb7d42a4
Dom <domeccleston@gmail.com>
2019-09-20 09:24:24 +0100

    added locations page

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -4,17 +4,17 @@ import CharacterCard from './CharacterCard';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
-  const [characterData, updateData] = useState([]);
+  const [characterData, updateCharacter] = useState([]);
 
-  useEffect(() => {
+ /*  useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
     axios.get("https://rickandmortyapi.com/api/character/")
       .then(response => {
-        updateData(response.data.results)
+        updateCharacter(response.data.results)
       })
       .catch(error => console.log(error))
-  }, []);
+  }, []); */
 
   return (
     <section className="character-list grid-view">
--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,5 +1,19 @@
 import React from "react";
+import { Card, Icon} from "semantic-ui-react";
 
-export default function LocationCard({ name, type, dimension, residents }) {
-  return <span>todo: location</span>;
+export default function LocationCard({data}) {
+  console.log(data)
+
+  return (
+    <Card>
+      <Card.Content
+      header={data.name}
+      meta={data.type}
+      description={data.dimension === 'unknown' ? "Unknown Dimension" : data.dimension}/>
+      <Card.Content extra>
+        <Icon name='user' />
+        {data.residents.length > 1 ? `${data.residents.length} residents` : `${data.residents.length} resident`}
+      </Card.Content>
+    </Card>
+  )
 }
